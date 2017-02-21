ca6fad206cdd2e5b1843f76706c65a39e568c699
Jana McKinnon <janamck007@gmail.com>
2017-02-21 12:45:00 -0500

    added retweet function

--- twitt.py
+++ twitt.py
@@ -8,16 +8,15 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-
-
-
-#open doc
-file = open('doc.txt', 'rU') #U for universal, \r\n same as \r
-#read lines
-lines = file.readlines()
-#close doc
-file.close()
-def tweet():
+#tweets a poem provided as a doc, line by line
+def tweetPoem():
+    #open doc
+    file = open('doc.txt', 'rU') #U for universal, \r\n same as \r
+    #read lines
+    lines = file.readlines()
+    #close doc
+    file.close()
+    
     for line in lines:
         if line != '\n' and line != '\r\n':
             try:
@@ -26,8 +25,19 @@ def tweet():
                 sleep(5)
             except:
                 print('duplicate status')
-                
-            
-tweet()
+
+#pass the username as paramter
+def reTweetStuff(userName):
+    #iterate through a specific users tweets, currently limited to 3 pages
+    i = 1
+    for status in tweepy.Cursor(api.user_timeline, id=userName).items(5):
+        # process status here
+        #process_status(status)
+        print(i)
+        i += 1
+        api.retweet(status.id)
+
+reTweetStuff('JoshuaAJarrett')
+#tweetPoem()
 
 
